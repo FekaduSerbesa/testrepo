da6e47c44a88fe6831cde3f0dadf0772fbd1d113
FekaduSerbesa <fike2006@yahoo.com>
2023-07-09 07:30:01 -0400

    first commit

---
+++ src/test/java/runners/RunnerClass.java
@@ -0,0 +1,29 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+//        features we use to provide the path of all the feature files
+        features ="src/test/resources/features",
+//        glue keyword we use to provide the path of the package where
+//        the step defs are available
+        glue= "steps",
+//        when dry run is equal to true it stops actual execution, it quickly scans
+//        and will provide the actual missing step definition
+        dryRun= false,
+
+        tags = " @sprint1",
+//        monochrome means sometimes the console output for cucumber test is having some
+//        irrelevant info, when you set it to true, it removes all the irrelevant info
+//        from the console and will give you simple output
+        monochrome = true,
+//        "pretty" is used to print all the steps in console
+        plugin={"pretty","html:target/cucumber.html","json:target/cucumber.json","rerun:target/failed.txt" }
+)
+
+public class RunnerClass {
+
+}
